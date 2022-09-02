715b6c66b864e3f87810edde2b80bb8bd550f764
lowkeyfish <hnyujunyang@gmail.com>
2022-09-02 15:58:34 +0800

    init

---
+++ iddd-springboot/src/main/java/com/yujunyang/iddd/cardealer/IDDDSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yujunyang.iddd.cardealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDDDSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IDDDSpringbootApplication.class, args);
+    }
+
+}
---
+++ iddd-springboot/src/test/java/com/yujunyang/iddd/cardealer/IdddSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujunyang.iddd.cardealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdddSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
